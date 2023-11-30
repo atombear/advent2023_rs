28eb1a520eea66e4bf080174a37b1582a055dea0
alexander papageorge <alexander.papageorge@openlabsusa.org>
2023-11-29 21:46:00 -0500

    scaffold

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+extern crate core;
+
+mod problem20231201;
+
+fn process_answer(answers: &mut Vec<String>, idx: usize, ans0: String, ans1: String) {
+    while answers.len() <= idx {
+        answers.push("".to_string());
+    }
+    assert_eq!(answers[idx], "");
+    answers[idx] = format!("{} {}", ans0, ans1);
+}
+
+fn main() {
+    let mut answers: Vec<String> = vec![];
+
+    // String, String
+    for daily_fn in [problem20231201::problem] {
+        let (idx, ans0, ans1) = daily_fn();
+        process_answer(&mut answers, idx, ans0, ans1);
+    }
+
+    for (idx, ans) in answers.iter().enumerate() {
+        println!("Day {} {}", idx + 1, ans);
+    }
+}
---
+++ src/problem20231201.rs
@@ -0,0 +1,3 @@
+pub fn problem() -> (usize, String, String) {
+    return (0, "Hello".to_string(), "There".to_string());
+}
